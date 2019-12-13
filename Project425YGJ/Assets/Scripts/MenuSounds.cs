7491b375b650e235cafb5fc1cbc8d71dcbbb2e3e
yamminezze <yezzedin@gmu.edu>
2019-12-13 13:02:25 -0500

    music and sound effects

---
+++ Project425YGJ/Assets/Scripts/MenuSounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSounds : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void HoverSound()
+    {
+        myFx.volume = 0.05f;
+        myFx.PlayOneShot(hoverFx);
+    }
+
+    public void ClickSound()
+    {
+        myFx.volume = 0.01f;
+        myFx.PlayOneShot(clickFx);
+    }
+}
